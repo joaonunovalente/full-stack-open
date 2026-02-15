d3e6b40a50248f135a5c76cf44eddb1ec3b11062
joaonunovalente <jnvalente@ua.pt>
2026-02-15 21:18:08 +0000

    Add 2.11: The Phonebook, Step 6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import FilterNameInput from "./components/FilterNameInput";
 import PersonForm from "./components/PersonForm";
 import ShowPersons from "./components/ShowPersons";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [personsToShow, setPersonsToShow] = useState(persons);
@@ -17,6 +13,16 @@ const App = () => {
   useEffect(() => {
     setPersonsToShow(persons);
   }, [persons]);
+
+  useEffect(() => {
+    console.log("The hook Axios is here...");
+    axios
+      .get("http://localhost:3001/persons")
+      .then((response) => {
+        console.log("promise fulfilled");
+        setPersons(response.data);
+      })
+  }, []);
 
   const addPersonForm = (event) => {
     event.preventDefault();
