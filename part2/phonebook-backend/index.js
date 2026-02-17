04a8ca2a5513856b80e21a74ab72d8b3132ea698
joaonunovalente <jnvalente@ua.pt>
2026-02-17 15:16:52 +0000

    Add 3.5: Phonebook backend, Step 5

--- part2/phonebook-backend/index.js
+++ part2/phonebook-backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json())
+
 let persons = [
   {
     id: "1",
@@ -60,6 +62,26 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end()
 })
 
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name is  missing',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number || "",
+    id: String(Math.floor(Math.random() * 100_000)),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(persons)
+})
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
