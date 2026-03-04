1a887924aa8577ee79a1d30159f5f8808b191a22
joaonunovalente <jnvalente@ua.pt>
2026-03-04 13:47:15 +0000

    Add 3.17: Phonebook database, Step 5

--- part3/phonebook-database/index.js
+++ part3/phonebook-database/index.js
@@ -9,14 +9,14 @@ app.use(express.json());
 app.use(express.static("dist"));
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
   }
 
-  next(error)
-}
+  next(error);
+};
 
 morgan.token("body", (req) => {
   return JSON.stringify(req.body);
@@ -59,10 +59,10 @@ app.get("/api/persons/:id", (request, response) => {
 app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id;
   Person.findByIdAndDelete(id)
-  .then((result) => {
-    response.status(204).end();
-  })
-  .catch(error => next(error))
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -97,13 +97,34 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id;
+  const { name, number } = request.body;
+
+  Person.findById(id)
+    .then((person) => {
+      if (!person) {
+        return response.status(404).end()
+      }
+      
+      person.name = name;
+      person.number = number;
+
+      return person
+        .save()
+        .then((updatedPerson) => {
+          response.json(updatedPerson);
+        })
+    .catch((error) => next(error));
+  });
+});
+
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
-app.use(errorHandler)
-
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
